316f9475dd8e86d3a50e4181befe04e77af198d7
Darya Dolidze <dolidze.darya@yandex.ru>
2024-10-23 16:09:46 +0300

    первая сдача

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,38 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template");
 
+const placeList = document.querySelector(".places__list");
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
+function createCard(cardTemplate, element, callback) {
+    const template = cardTemplate.content;
+    cardElement = template.querySelector(".places__item").cloneNode(true);
+
+    const cardImage = cardElement.querySelector(".card__image");
+    cardImage.setAttribute("src", element.link);
+    cardImage.setAttribute("alt", element.name);
+    cardElement.querySelector(".card__title").textContent = element.name;
+
+    const deleteButton = cardElement.querySelector(".card__delete-button");
+    deleteButton.addEventListener("click", callback);
+
+    return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(evt) {
+    const evtTarget = evt.target;
+    const listItem = evtTarget.closest(".places__item");
+    listItem.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((element,) => {
+    const cardElement = createCard(
+    cardTemplate,
+    element,
+    deleteCard
+    );
+
+    placeList.append(cardElement);
+})
